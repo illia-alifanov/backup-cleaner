b52f26d755b441e2eb1a51f09f32a5ae24aefdff
Illia Alifanov <illia.alifanov@eleks.com>
2025-03-22 15:01:21 +0200

    initial commit

---
+++ ClearBackups/ExtraDiffsCleaner.cs
@@ -0,0 +1,48 @@
+﻿namespace ClearBackups
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Globalization;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+    using System.Text.RegularExpressions;
+    using System.Threading.Tasks;
+
+    internal class ExtraDiffsCleaner(IFileHelper fileHelper)
+    {
+        
+        public void Run(string folder)
+        {
+            Regex reg = new Regex(@"_diff\.bak$");
+
+            var files = Directory.GetFiles(folder, "*.bak", SearchOption.AllDirectories)
+                                 .Where(path => reg.IsMatch(path))
+                                 .ToList();
+
+            var fileDict = new Dictionary<DateTime, List<FileModel>>();
+            foreach (var file in files)
+            {
+                Console.WriteLine(file);
+                var date = fileHelper.GetDate(Path.GetFileName(file));
+
+                fileHelper.AddFile(fileDict, date, file);
+            }
+
+
+            foreach (var key in fileDict.Keys)
+            {
+                var list = fileDict[key];
+                for (int i = 0; i < list.Count - 2; i++)
+                {
+                    if (!string.IsNullOrEmpty(list[i].Path) && File.Exists(list[i].Path))
+                    {
+                        File.Delete(list[i].Path!);
+                    }
+
+                }
+
+            }
+        }
+    }
+}
---
+++ ClearBackups/FileHelper.cs
@@ -0,0 +1,66 @@
+﻿namespace ClearBackups
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Globalization;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    interface IFileHelper
+    {
+        DateTime GetDate(string file);
+        string GetBaseAndDateName(string file);
+        string GetBaseAndNextDateName(string file);
+        void AddFile(Dictionary<DateTime, List<FileModel>> dict, DateTime date, string file);
+        
+    }
+
+    internal class FileHelper : IFileHelper
+    {
+        public void AddFile(Dictionary<DateTime, List<FileModel>> dict, DateTime date, string file)
+        {
+            var list = dict.ContainsKey(date.Date) ? dict[date.Date] : [];
+            list.Add(new FileModel { Date = date, Path = file });
+
+            dict[date.Date] = list;
+        }
+
+        public DateTime GetDate(string file)
+        {
+            var fileNameParts = file.Split('_');
+            string dateString = fileNameParts[1];
+            string datePart = dateString.Split('T')[0];
+            string timeString = dateString.Split('T')[1];
+            string timePart = timeString.Replace('-', ':');
+            dateString = $"{datePart}T{timePart}:00";
+
+            DateTime date = DateTime.Parse(dateString, CultureInfo.InvariantCulture, DateTimeStyles.AssumeLocal);
+
+            return date;
+        }
+
+        public string GetBaseAndDateName(string file)
+        {
+            var fileNameParts = file.Split('_');
+            string dateString = fileNameParts[1];
+            string datePart = dateString.Split('T')[0];
+            string folderName = $"{fileNameParts[0]}_{datePart}";
+
+            return folderName;
+        }
+
+        public string GetBaseAndNextDateName(string file)
+        {
+            var fileNameParts = file.Split('_');
+            string dateString = fileNameParts[1];
+            string datePart = dateString.Split('T')[0];
+            DateTime date = DateTime.Parse(datePart, CultureInfo.InvariantCulture, DateTimeStyles.AssumeLocal);
+            DateTime nextDate = date.Date.AddDays(1);
+
+            string folderName = $"{fileNameParts[0]}_{nextDate.ToString("yyyy-MM-dd")}";
+
+            return folderName;
+        }
+    }
+}
